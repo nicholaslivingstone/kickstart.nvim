59dd701ae51ade5abdfc629c931786e00b489eb4
Nicholas Livingstone <nlivingsto@unm.edu>
2025-03-25 16:10:03 -0600

    Added ormolu as a haskell formatter for conform.nvim

--- init.lua
+++ init.lua
@@ -768,7 +768,7 @@ require('lazy').setup({
         lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
-        --
+        haskell = { 'ormolu' },
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
       },
