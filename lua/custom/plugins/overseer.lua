998eb528ca52a7fc42e15e48dbfc63b809de80d7
Nicholas Livingstone <nlivingsto@unm.edu>
2025-11-24 14:05:22 -0700

    Adding overseer plugin

--- lua/custom/plugins/overseer.lua
+++ lua/custom/plugins/overseer.lua
@@ -1,4 +1,13 @@
 return {
   'stevearc/overseer.nvim',
-  opts = {},
+  opts = {
+    templates = { 'builtin' },
+    dap = false,
+  },
+  cmd = { 'OverseerRun', 'OverseerToggle', 'OverseerInfo' },
+  keys = {
+    { '<leader>or', '<cmd>OverseerRun<cr>', desc = 'Run task' },
+    { '<leader>ot', '<cmd>OverseerToggle<cr>', desc = 'Toggle task list' },
+    { '<leader>oi', '<cmd>OverseerInfo<cr>', desc = 'Overseer Info' },
+  },
 }
