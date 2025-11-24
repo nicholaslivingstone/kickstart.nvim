8515ff37c167ba166887b56156e67ae34c9883db
Nicholas Livingstone <nlivingsto@unm.edu>
2025-11-24 15:04:21 -0700

    Modifying toggleterm to refresh gitsigns when closing lazy git

--- lua/custom/plugins/toggleterm.lua
+++ lua/custom/plugins/toggleterm.lua
@@ -13,14 +13,22 @@ return {
         border = 'curved',
       },
     },
-    init = function()
+    config = function(_, opts)
+      require('toggleterm').setup(opts)
+
       local Terminal = require('toggleterm.terminal').Terminal
       local lazygit = Terminal:new {
         cmd = 'lazygit',
-        display_name = ' LazyGit',
+        display_name = ' LazyGit',
         hidden = true,
         direction = 'float',
         close_on_exit = true,
+        on_close = function()
+          -- Refresh gitsigns after closing lazygit
+          vim.defer_fn(function()
+            vim.cmd 'silent! Gitsigns refresh'
+          end, 100)
+        end,
       }
 
       vim.keymap.set('n', '<leader>g', function()
